d3ea6c4a6ee0ba4a09f066947d07609775f6359f
lukesmith <stuff@lukesmith.net>
2009-07-16 21:41:57 +0000

    Added documentation

--- src/Lix.Commons/Repositories/IUnitOfWork.cs
+++ src/Lix.Commons/Repositories/IUnitOfWork.cs
@@ -2,11 +2,30 @@ using System;
 
 namespace Lix.Commons.Repositories
 {
+    /// <summary>
+    /// Represents a unit of work.
+    /// </summary>
     public interface IUnitOfWork : IDisposable
     {
-        void Begin();
-        void Commit();
-        void Rollback();
+        /// <summary>
+        /// Gets a value indicating whether unit of work is active.
+        /// </summary>
+        /// <value><c>true</c> if this instance is active; otherwise, <c>false</c>.</value>
         bool IsActive { get; }
+
+        /// <summary>
+        /// Begins a new unit of work.
+        /// </summary>
+        void Begin();
+
+        /// <summary>
+        /// Commits the unit of work.
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Rollbacks the unit of work.
+        /// </summary>
+        void Rollback();
     }
 }
