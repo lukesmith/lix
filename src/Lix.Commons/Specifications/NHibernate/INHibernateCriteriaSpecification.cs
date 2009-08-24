25539311195671dd2810bacc2753ec7f65e4c454
lukesmith <stuff@lukesmith.net>
2009-08-24 21:49:49 +0000

    Created an INHibernateSpecification

---
+++ src/Lix.Commons/Specifications/NHibernate/INHibernateCriteriaSpecification.cs
@@ -0,0 +1,11 @@
+using NHibernate;
+
+namespace Lix.Commons.Specifications.NHibernate
+{
+    /// <summary>
+    /// Represents an NHibernate <see cref="ICriteria"/> specification.
+    /// </summary>
+    public interface INHibernateCriteriaSpecification : INHibernateSpecification<ICriteria>
+    {
+    }
+}
--- src/Lix.Commons/Specifications/NHibernate/INHibernateSpecification.cs
+++ src/Lix.Commons/Specifications/NHibernate/INHibernateSpecification.cs
@@ -3,9 +3,10 @@ using NHibernate;
 namespace Lix.Commons.Specifications.NHibernate
 {
     /// <summary>
-    /// Represents an NHibernate <see cref="ICriteria"/> specification.
+    /// Represents an NHibernate specification.
     /// </summary>
-    public interface INHibernateCriteriaSpecification : ISpecification<ISession, ICriteria>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    public interface INHibernateSpecification<TResult> : ISpecification<ISession, TResult>
     {
     }
 }
