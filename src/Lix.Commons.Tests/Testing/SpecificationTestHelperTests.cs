866481f9dcd787489382ea3e8b077c987f89a6b3
lukesmith <stuff@lukesmith.net>
2009-10-08 21:38:32 +0000

    Added helper class for testing IQueryableSpecifications

---
+++ src/Lix.Commons.Tests/Testing/SpecificationTestHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lix.Commons.Testing;
+using Lix.Commons.Tests.Examples;
+using Lix.Commons.Tests.Examples.Specifications;
+using MbUnit.Framework;
+using Moq;
+
+namespace Lix.Commons.Tests.Testing
+{
+    [TestFixture]
+    public class SpecificationTestHelperTests
+    {
+        [Test]
+        public void should_build_the_specification()
+        {
+            var specification = new Mock<FindFishDescriptionContainingSpecification>("smelly");
+
+            specification.Setup(x => x.Build(It.IsAny<IQueryable<Fish>>()));
+
+            SpecificationTestHelper.Test(specification.Object, new List<Fish>());
+
+            specification.Verify();
+        }
+
+        [Test]
+        public void should_return_an_empty_result_when_the_specification_is_null()
+        {
+            var result = SpecificationTestHelper.Test(null, new List<Fish>());
+
+            result.Count().ShouldBeEqualTo(0);
+        }
+
+        [Test]
+        public void should_return_an_empty_result_when_the_source_data_is_null()
+        {
+            var result = SpecificationTestHelper.Test(new FindFishWithDescriptionSpecification("smelly"), null);
+
+            result.Count().ShouldBeEqualTo(0);
+        }
+    }
+}
---
+++ src/Lix.Commons/Testing/SpecificationTestHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lix.Commons.Specifications;
+
+namespace Lix.Commons.Testing
+{
+    /// <summary>
+    /// Represents a helper class for testing specifications.
+    /// </summary>
+    public static class SpecificationTestHelper
+    {
+        /// <summary>
+        /// Tests a the <paramref name="specification"/> against the <paramref name="sourceData"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="specification">The specification to test.</param>
+        /// <param name="sourceData">The data to test the specification against.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{TEntity}"/> of the specification result.
+        /// </returns>
+        public static IEnumerable<TEntity> Test<TEntity>(IQueryableSpecification<TEntity> specification, IEnumerable<TEntity> sourceData)
+        {
+            if (specification == null)
+            {
+                return Enumerable.Empty<TEntity>();
+            }
+
+            if (sourceData == null)
+            {
+                return Enumerable.Empty<TEntity>();
+            }
+
+            var result = specification.Build(sourceData.AsQueryable());
+
+            return result;
+        }
+    }
+}
