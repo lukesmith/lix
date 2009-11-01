ea584a596a55d820e20baf7452b4a97302e658ea
lukesmith <stuff@lukesmith.net>
2009-11-01 14:24:58 +0000

    Modified build script

--- BuildUtils.rb
+++ BuildUtils.rb
@@ -1,27 +1,5 @@
 require 'erb'
 
-class NUnitRunner
-	include FileTest
-
-	def initialize(paths)
-		@sourceDir = paths.fetch(:source, 'source')
-		@resultsDir = paths.fetch(:results, 'results')
-		@compilePlatform = paths.fetch(:platform, '')
-		@compileTarget = paths.fetch(:compilemode, 'debug')
-	
-		@nunitExe = File.join('lib', 'nunit', "nunit-console#{(@compilePlatform.empty? ? '' : "-#{@compilePlatform}")}.exe").gsub('/','\\') + ' /nothread'
-	end
-	
-	def executeTests(assemblies)
-		Dir.mkdir @resultsDir unless exists?(@resultsDir)
-		
-		assemblies.each do |assem|
-			file = File.expand_path("#{@sourceDir}/#{assem}/bin/#{@compileTarget}/#{assem}.dll")
-			sh "#{@nunitExe} #{file}"
-		end
-	end
-end
-
 class MbUnitRunner
 	include FileTest
 
@@ -53,34 +31,18 @@ class MSBuildRunner
 		frameworkDir = File.join(ENV['windir'].dup, 'Microsoft.NET', 'Framework', version)
 		msbuildFile = File.join(frameworkDir, 'msbuild.exe')
 		
-		sh "#{msbuildFile} #{solutionFile} /nologo /maxcpucount /v:m /property:BuildInParallel=false /property:Configuration=#{compileTarget} /t:Rebuild"
-	end
-end
-
-class AspNetCompilerRunner
-	def self.compile(attributes)
-		
-		webPhysDir = attributes.fetch(:webPhysDir, '')
-		webVirDir = attributes.fetch(:webVirDir, 'This_Value_Is_Not_Used')
-		
-		frameworkDir = File.join(ENV['windir'].dup, 'Microsoft.NET', 'Framework', 'v2.0.50727')
-		aspNetCompiler = File.join(frameworkDir, 'aspnet_compiler.exe')
-
-		sh "#{aspNetCompiler} -nologo -errorstack -c -p #{webPhysDir} -v #{webVirDir}"
+		sh "#{msbuildFile} #{solutionFile} /nologo /maxcpucount /v:m /property:BuildInParallel=true /property:Configuration=#{compileTarget} /t:Rebuild"
 	end
 end
 
 class AsmInfoBuilder
 	attr_reader :buildnumber
 
-	def initialize(baseVersion, properties)
+	def initialize(version, properties)
 		@properties = properties;
-		
-		@buildnumber = baseVersion + (ENV["CCNetLabel"].nil? ? '0' : ENV["CCNetLabel"].to_s)
+		@buildnumber = version
 		@properties['Version'] = @properties['InformationalVersion'] = buildnumber;
 	end
-
-
 	
 	def write(file)
 		template = %q{
