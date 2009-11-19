16d1db8f997ffc8df37fa7deaf01b076245eb683
lukesmith <stuff@lukesmith.net>
2009-11-19 20:34:07 +0000

    Marked IRepository as Obsolete Supress warning number 618 (Obsolete)

--- src/Lix.Commons/Repositories/IRepository.cs
+++ src/Lix.Commons/Repositories/IRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace Lix.Commons.Repositories
+﻿using System;
+
+namespace Lix.Commons.Repositories
 {
     /// <summary>
     /// Represents a generic repository
     /// </summary>
     /// <typeparam name="TEntity">Type of the entity the repository is for.</typeparam>
+    [Obsolete("Use specific repository, either ICommandRepository<> or IQueryRepository<>")]
     public interface IRepository<TEntity> : ICommandRepository<TEntity>, IQueryRepository<TEntity>
     {
     }
