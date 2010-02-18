2fda4c05d019ddd4bf835162f41087f73d4e91de
lukesmith <stuff@lukesmith.net>
2010-02-18 00:05:56 +0000

    Updated project files with consistent mapping to StyleCop target. Added RakeBuild property passed to msbuild

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,8 +2,9 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Lix")]
+[assembly: AssemblyDescription("this is the assembly description")]
 [assembly: AssemblyCompany("Lix")]
 [assembly: AssemblyProduct("Lix")]
-[assembly: AssemblyCopyright("copyright 2010, Luke Smith")]
-[assembly: AssemblyVersion("0.0.0.2")]
+[assembly: AssemblyCopyright("copyright 2009, Luke Smith")]
+[assembly: AssemblyVersion("0.0.0.1")]
 
