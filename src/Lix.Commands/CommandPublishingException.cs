c03f74360ef9f3601325c6f6b6e154340feaf519
lukesmith <stuff@lukesmith.net>
2010-03-20 16:09:29 +0000

    Added serialization support

--- src/Lix.Commands/CommandPublishingException.cs
+++ src/Lix.Commands/CommandPublishingException.cs
@@ -1,11 +1,26 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Lix.Commands
 {
+    [Serializable]
     public class CommandPublishingException : Exception
     {
-        public CommandPublishingException(string message)
-            : base(message)
+        public CommandPublishingException()
+        {
+        }
+
+        public CommandPublishingException(string message) : base(message)
+        {
+        }
+
+        public CommandPublishingException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected CommandPublishingException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
         {
         }
     }
--- src/Lix.Commands/InvalidCommandException.cs
+++ src/Lix.Commands/InvalidCommandException.cs
@@ -1,14 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Lix.Commands
 {
+    [Serializable]
     public class InvalidCommandException : Exception
     {
         public InvalidCommandException(ICommand command, IEnumerable<CommandValidationRule> validationRules)
         {
             this.Command = command;
             this.ValidationRules = validationRules;
+        }
+
+        public InvalidCommandException(string message) : base(message)
+        {
+        }
+
+        public InvalidCommandException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected InvalidCommandException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
         }
 
         public ICommand Command
